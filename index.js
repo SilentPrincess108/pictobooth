c76d427a78af44ed040580e556bd5daf2d8dc483
Rosan <rosan.syal@gmail.com>
2026-08-15 19:39:00 -0400

    imlemented taking pics and downloading pics

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 const webcamVideo = document.getElementById("webcam-video");
 
-navigator.mediaDevices.getUserMedia({video: true}).then((stream) => {
+const canvas = document.getElementById("picture");
+const ctx = canvas.getContext("2d");
+
+let photos = [];
+
+const constraints = {
+    video: {
+        width: 500,
+        height: 400,
+    }
+};
+
+navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
     webcamVideo.srcObject = stream;
-}).catch((error) => {
-    console.error(error);
-    //add error pop up window
 })
+.catch((error) => {
+    console.error(error);
+    alert("Heyy, I need access to your cam so I can see your beautiful face (｡•̀ᴗ-)✧")
+})
+
+function snap(){
+    ctx.drawImage(webcamVideo, 0, 0, canvas.width, canvas.height);
+    photos.push(canvas.toDataURL("image/png"))
+}
+
+function download(){
+    const imgURL = canvas.toDataURL("image/png");
+    const link = document.createElement("a");
+    link.href = imgURL;
+    link.download = "selfie.png";
+    link.click();
+    document.removeChild(link);
+}
